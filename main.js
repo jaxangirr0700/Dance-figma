75d701c872aac467d0bf2503e11c73db2835f249
jaxangir <jaxangirr0700@gmail.com>
2024-11-03 14:59:32 +0500

    finished 80%

---
+++ main.js
@@ -0,0 +1,62 @@
+// 1
+//
+// let arr = ["olma", "nok", "banan", "olma", "nok", "tarvuz"];
+// function deleteMeva(name) {
+//   arr = arr.filter((item) => item != name);
+// }
+// deleteMeva("olma");
+// console.log(arr);
+//
+// 2
+//
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(Math.max(...arr), Math.min(...arr));
+//
+//
+// 3
+//
+// let arr = "AAABBCCCA";
+// function decodeString(encoded) {
+//   return encoded.replace((match, char, count) => {
+//     return char.repeat(count ? Number(count) : 1);
+//   });
+// }
+
+// // Misol
+// let encodedStr = "A3B2C3A";
+// let decodedStr = decodeString(encodedStr);
+// console.log(decodedStr); // AAABBCCCA
+
+// 6
+//
+// let num = "467812";
+// num = num.split("").map(Number);
+// // function fn(a, b) {
+
+// // }
+// console.log(Math.max(...num), Math.min(...num));
+//
+// 8
+//
+// let soat = "10:15AM";
+// if ((soat = "AM")) {
+//   soat = "22:15";
+// } else if ((soat = "PM")) {
+//   soat = "8:30";
+// }
+// console.log(soat);
+
+//
+// 9
+// let str1 = "web";
+// let str2 = "brain";
+// str1 = str1 + str2;
+// str2 = str2 + str1;
+
+// str2 = str2.slice(5, 8);
+// // str2 = str2.slice(-1, 5);
+// // console.log(str1);
+
+// str1 = str1.slice(3);
+
+// console.log(str1, str2);
